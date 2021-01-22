2fe08f6a4c005d84623f6f934f2b40715a7821c0
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-22 09:26:54 +0200

    all done

--- rest-countries-api-with-color-theme-switcher/main.js
+++ rest-countries-api-with-color-theme-switcher/main.js
@@ -3,6 +3,9 @@ const search = document.getElementById('search')
 const selectCountries = document.getElementById('select-countries')
 const toggle = document.getElementById('toggle')
 const body = document.querySelector('body');
+const form = document.getElementById('form');
+const countrySection = document.getElementById('country')
+
 // EventListeners
 
 search.addEventListener('input',searchCountry)
@@ -23,8 +26,8 @@ async function getCountries() {
 
 function showCountries(countries) {
   countries.forEach(country => {
-    container.innerHTML += `<a href="#countries">
-    <div class="country" data-topLevelDomain="${country.topLevelDomain}">
+    container.innerHTML += `
+    <div class="country" data-alpha3Code="${country.alpha3Code}">
     
     <div class="flag">
       <img src=${country.flag} alt="" />
@@ -40,7 +43,6 @@ function showCountries(countries) {
       </div>
     </div>
   </div>
-  </a>
     `
   });
 }
@@ -85,8 +87,69 @@ function toggleMode() {
 } 
 
 function countryInfo(country) {
-  console.log(country)
+  container.style.display='none'
+  form.style.display='none'
+  console.log(country);
+  getCountry(country)
  
+}
+
+async function getCountry(alpha3Code) {
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${alpha3Code}`)
+
+  const output = await res.json()
+  showCountry(output)
+}
+
+
+function showCountry(country) {
+
+  countrySection.innerHTML = `
+  
+  <div class="back">
+        <a href="" class="btn-back"><i class="fas fa-chevron-left"></i>Back</a>
+
+      </div>
+      <div class="country-info">
+        <div class="photo">
+          <img src="${country.flag}" alt="">
+        </div>
+        <div class="data">
+          <div class="name">${country.name}</div>
+          <div class="details">
+            <p>Native Name:<span>${country.nativeName}</span></p>
+            <p>Population:<span>${country.population}</span></p>
+            <p>Region:<span>${country.region}</span></p>
+            <p>SubRegion:<span>${country.subregion}</span></p>
+            <p>Capital:<span>${country.capital}</span></p>
+            <p>Top Level Domain: <span>${country.topLevelDomain}</span></p>
+            <p>currencies: <span id="currencies" ></span></p>
+            <p>Languages:<span id="languages"></span></p>
+          </div>
+          <div class="tags">
+            <h2>Borders Countries:</h2>
+
+          </div>
+        </div>
+      </div>
+  
+  `
+  const borders = country.borders
+  const currencies = country.currencies
+  const languages = country.languages
+  const currenciesEl = document.getElementById('currencies')
+  const languagesEl = document.getElementById('languages')
+  const tags = document.querySelector('.tags')
+  currencies.forEach(item => {
+    currenciesEl.innerHTML += `${item.name}, `
+  })
+
+  languages.forEach(item => {
+    languagesEl.innerHTML += `${item.name}, `
+  })
+  borders.forEach(border => {
+    tags.innerHTML += `<span class="borders">${border}</span>`
+  })
 }
 
 container.addEventListener("click", (e) => {
@@ -99,8 +162,8 @@ container.addEventListener("click", (e) => {
   });
 
   if (country) {
-    const topLevelDomain = country.getAttribute("data-topLevelDomain");
-    console.log(topLevelDomain);
+    const alpha3Code = country.getAttribute("data-alpha3Code");
+    countryInfo(alpha3Code);
   }
 });
 
@@ -110,8 +173,39 @@ container.addEventListener("click", (e) => {
 
 
 
+/*
 
+<div class="back">
+        <a href="" class="btn-back"><i class="fas fa-chevron-left"></i>Back</a>
 
+      </div>
+      <div class="country-info">
+        <div class="photo">
+          <img src="css/flag.png" alt="">
+        </div>
+        <div class="data">
+          <div class="name">Egypt</div>
+          <div class="details">
+            <p>Native Name:<span>dd</span></p>
+            <p>Population:<span>dd</span></p>
+            <p>Region:<span>dd</span></p>
+            <p>SubRegion:<span>dd</span></p>
+            <p>Capital:<span>dd</span></p>
+            <p>Top Level Domain: <span>dd</span></p>
+            <p>currencies<span>dd</span></p>
+            <p>Languages:<span>dd</span></p>
+          </div>
+          <div class="tags">
+            <h2>Borders Countries:</h2>
+            <span class="borders">ddd</span>
+            <span class="borders">dddd</span>
+            <span class="borders">ddd</span>
+            <span class="borders">ddd</span>
+            <span class="borders">ddd</span>
+          </div>
+        </div>
+      </div>
+*/
 
 
 
