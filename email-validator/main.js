02efd4362bd39db62ead789431929f2dd453f6b5
MhmodTayel <mhmodtayel0050@gmail.com>
2021-02-11 20:38:42 +0200

    add error message

--- email-validator/main.js
+++ email-validator/main.js
@@ -2,16 +2,28 @@ const input = document.getElementById("input");
 const inputBtn = document.getElementById("input-btn");
 const outputData = document.getElementById("output-data");
 const outputInfo = document.getElementById("output-info");
-inputBtn.addEventListener("click", getData);
-async function getData() {
+inputBtn.addEventListener("click", () => {
+  let email = input.value;
+  if (!email) {
+    Swal.fire({
+      icon: "error",
+      title: "Please, Enter an Email..",
+    });
+  } else {
+    getData(email);
+  }
+});
+async function getData(email) {
   const res = await fetch(
-    `https://api.mailboxvalidator.com/v1/validation/single?email=mhmodtayel9848@gmail.com&key=1MA8C2LTZASZQGV6ZA78&format=json`
+    `https://api.mailboxvalidator.com/v1/validation/single?email=${email}&key=1MA8C2LTZASZQGV6ZA78&format=json`
   );
   const output = await res.json();
   showOutput(output);
 }
 
 function showOutput(data) {
+  outputData.innerHTML = "";
+  input.value = "";
   Object.keys(data)
     .filter((key) => key.includes("is"))
     .forEach((item) => {
